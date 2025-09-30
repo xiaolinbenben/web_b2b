65e63a82a98c8277e9ada7a242204dcbb08450a2
林智清 <3370942916@qq.com>
2025-09-30 14:38:44 +0800

    Reapply "fix: 更新生产环境配置"
    This reverts commit 2cfc8a7120d8495bc0be6e4dfd2f31cd6c9a2c62.

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from server import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
+    re_path(r'^upload/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
